998aafc0c87699ec520a32091a1bc9dfb2d19dcb
Ana-Vartasashvili <anavartasashvili30@gmail.com>
2022-09-08 14:14:10 +0400

    add animations to sidebar and close icons

--- src/components/icons/CloseIcon.js
+++ src/components/icons/CloseIcon.js
@@ -1,11 +1,6 @@
 const CloseIcon = () => {
   return (
-    <svg
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth="2.5"
-      stroke="currentColor"
-    >
+    <svg fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor">
       <path
         strokeLinecap="round"
         strokeLinejoin="round"
--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,5 +1,5 @@
 import classes from './Sidebar.module.css'
-import sidebarIllustration from '../../images/Chubbs_11-DrawKit_Vector_Illustrations_.png'
+
 import CloseIcon from '../icons/CloseIcon'
 
 const SideBar = (props) => {
@@ -9,21 +9,19 @@ const SideBar = (props) => {
 
   return (
     <>
-      <div className={classes.sidebar}>
-        <div className={classes.closeIcon} onClick={closeSidebarHandler}>
-          <CloseIcon />
+      <div className={`${classes.sidebar} ${classes.slide} `}>
+        <div className={classes.sidebarContainer}>
+          <div className={classes.closeIcon} onClick={closeSidebarHandler}>
+            <CloseIcon />
+          </div>
+          <ul>
+            <li>HOME</li>
+            <li>SHOP</li>
+            <li>ABOUT</li>
+          </ul>
         </div>
-        <ul>
-          <li>HOME</li>
-          <li>SHOP</li>
-          <li>ABOUT</li>
-          <img
-            src={sidebarIllustration}
-            alt="cute illustration of a character drinking"
-          />
-        </ul>
-        {/* <div className={classes.overlayBlur}></div> */}
       </div>
+      <div className={classes.overlayBlur}></div>
     </>
   )
 }
