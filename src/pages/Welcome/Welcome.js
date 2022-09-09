512cfaacafa058ad904b0e5365b7421ee01ec791
Ana-Vartasashvili <anavartasashvili30@gmail.com>
2022-09-09 15:39:48 +0400

    make hero section responsive

--- src/pages/Welcome/Welcome.js
+++ src/pages/Welcome/Welcome.js
@@ -4,17 +4,19 @@ import heroImg from '../../images/hero-image.jpg'
 const Welcome = () => {
   return (
     <section className={classes.hero}>
-      <div className={classes.text}>
-        <h1 className={classes.heroTitle}>
-          <span className={classes.titleWords}>Best solution</span> for small
-          hunger.
-        </h1>
-        <p className={classes.heroSubtitle}>
-          We'll never compromise on our high quality standards.
-        </p>
-      </div>
-      <div className={classes.heroImg}>
-        <img src={heroImg} alt="corn flakes with different flavours" />
+      <div className={classes.heroContent}>
+        <div className={classes.text}>
+          <h1 className={classes.heroTitle}>
+            <span className={classes.titleWords}>Best solution</span> for small
+            hunger.
+          </h1>
+          <p className={classes.heroSubtitle}>
+            We'll never compromise on our high quality standards.
+          </p>
+        </div>
+        <div className={classes.heroImg}>
+          <img src={heroImg} alt="corn flakes with different flavours" />
+        </div>
       </div>
     </section>
   )
