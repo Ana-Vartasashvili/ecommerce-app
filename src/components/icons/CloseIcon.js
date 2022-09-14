f18de35c5031fcdd18f7dcab065fe41d3464eee9
Ana-Vartasashvili <anavartasashvili30@gmail.com>
2022-09-14 22:21:20 +0400

    add animation to cart close icon

--- src/components/icons/CloseIcon.js
+++ src/components/icons/CloseIcon.js
@@ -1,6 +1,11 @@
 const CloseIcon = () => {
   return (
-    <svg fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor">
+    <svg
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+    >
       <path
         strokeLinecap="round"
         strokeLinejoin="round"
