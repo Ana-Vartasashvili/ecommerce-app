ebdcbb53f6a3a474d76232a772e8a5ef4dc01300
Ana-Vartasashvili <anavartasashvili30@gmail.com>
2022-09-17 13:40:47 +0400

    create cart item layout

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -7,24 +7,26 @@ const CartItem = (props) => {
       <div className={classes.item}>
         <img src={props.image} alt="corn flakes" />
 
-        <div className={classes.titleQuantity}>
-          <p className={classes.title}>{props.title}</p>
-          <div className={classes.quantity}>
-            <span>QTY:</span>
-            <button>-</button>
-            <span>1</span>
-            <button>+</button>
-          </div>
-        </div>
-
-        <div className={classes.priceRemove}>
-          <p>{`$${props.price.toFixed(2)}`}</p>
-          <button className={classes.removeBtn}>
-            <div className={classes.removeIcon}>
-              <CloseIcon />
+        <div className={classes.text}>
+          <div className={classes.titleQuantity}>
+            <p className={classes.title}>{props.title}</p>
+            <div className={classes.quantity}>
+              <span>QTY:</span>
+              <button>-</button>
+              <span>1</span>
+              <button>+</button>
             </div>
-            <span> Remove</span>
-          </button>
+          </div>
+
+          <div className={classes.priceRemove}>
+            <p>{`$${props.price.toFixed(2)}`}</p>
+            <button className={classes.removeBtn}>
+              <div className={classes.removeIcon}>
+                <CloseIcon />
+              </div>
+              <span> Remove</span>
+            </button>
+          </div>
         </div>
       </div>
     </div>
